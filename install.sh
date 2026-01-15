4907ce6491e5b23c9735cdf6fba7cacede174ac0
Joachim Meyer <jmeyer@cs.uni-saarland.de>
2026-01-15 20:15:29 +0100

    Add nextcloud sync service.

--- install.sh
+++ install.sh
@@ -4,9 +4,18 @@ mkdir -p /usr/local/bin
 cp bin/* /usr/local/bin
 
 cp etc/systemd/system/capiture.service /etc/systemd/system/
+cp etc/systemd/system/nextcloud-sync-capiture.service /etc/systemd/system/
+cp etc/systemd/system/nextcloud-sync-capiture.timer /etc/systemd/system/
+cp etc/systemd/system/nextcloud-sync.slice /etc/systemd/system/
+
 systemctl daemon-reload
+
 systemctl enable capiture.service
 systemctl start capiture.service
+
+systemctl enable nextcloud-sync-capiture.timer
+systemctl start nextcloud-sync-capiture.timer
+
 
 echo "caPiture installed and service started."
 echo "Use 'systemctl status capiture.service' to check the service status."
